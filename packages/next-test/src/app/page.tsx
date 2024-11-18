8b6e68fbedd9366682e5b80a53a2d10b2a7a25dd
jimao <shihao.gu@perfma.com>
2024-11-18 20:14:03 +0800

    提交

--- packages/next-test/src/app/page.tsx
+++ packages/next-test/src/app/page.tsx
@@ -1,6 +1,12 @@
 import Box from "@/components/Box";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    console.log('你好，世界');
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
