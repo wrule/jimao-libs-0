c3f12b57bc644e8304cc1def7c127414a8ac7e27
jimao <shihao.gu@perfma.com>
2024-11-18 19:31:26 +0800

    调整代码

--- packages/console-test/src/index.ts
+++ packages/console-test/src/index.ts
@@ -1,10 +1,9 @@
 import dayjs from 'dayjs';
 import { nanoid } from 'nanoid';
 
-function main() {
+export
+const main = () => {
   setInterval(() => {
     console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'), nanoid());
   }, 1000);
-}
-
-main();
+};
