e654f0c5f2a45a143e7842e40ab43440bcf9b94f
jimao <shihao.gu@perfma.com>
2024-11-18 19:51:27 +0800

    提交

--- packages/console-test/src/index.ts
+++ packages/console-test/src/index.ts
@@ -7,3 +7,13 @@ const main = () => {
     console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'), nanoid());
   }, 1000);
 };
+
+export default
+class ABC {
+  public constructor(private uid = nanoid()) { }
+
+  public show() {
+    console.log('uid is', this.uid);
+    document.write('uid is ' + this.uid);
+  }
+}
